45343a23d57074e84ba3dc1bd697a1af1e4f5da9
ravi raushan <ravi@macair.local>
2022-12-03 00:23:38 +0530

    sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
+import Sidebar from "./Sidebar";
+
 const Feed = () => {
     return (
         <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
@@ -10,7 +12,7 @@ const Feed = () => {
                     borderRight: "1px solid #3d3d3d",
                     px: { sx: 0, md: 2 },
                 }}>
-                Sidebar
+                <Sidebar />
                 <Typography
                     className='copyright'
                     variant='body2'
