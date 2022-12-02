24c11c50af278225b8dee538ed5c2c7c389b5696
ravi raushan <ravi@macair.local>
2022-12-03 01:42:11 +0530

    video and channel card

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,8 +6,12 @@ import { Sidebar, Videos } from "./";
 
 const Feed = () => {
     const [selectedCategory, setSelectedCategory] = useState("New");
+    const [videos, setVideos] = useState([]);
+
     useEffect(() => {
-        fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+        fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+            setVideos(data.items)
+        );
     }, [selectedCategory]);
 
     return (
@@ -39,7 +43,7 @@ const Feed = () => {
                     {selectedCategory}{" "}
                     <span style={{ color: "#F31503" }}>videos</span>
                 </Typography>
-                <Videos videos={[]} />
+                <Videos videos={videos} />
             </Box>
         </Stack>
     );
