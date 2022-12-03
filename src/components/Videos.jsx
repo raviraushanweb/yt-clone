906eb06f67269dc811d5a502a58cce1a3f8cfd03
ravi raushan <raviraushanweb@gmail.com>
2022-12-03 19:11:06 +0530

    video player

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,10 +1,18 @@
-import { Stack, Box } from "@mui/system";
+import React from "react";
+import { Stack, Box } from "@mui/material";
 
-import { VideoCard, ChannelCard } from "./";
+import { ChannelCard, Loader, VideoCard } from "./";
 
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
+    if (!videos?.length) return <Loader />;
+
     return (
-        <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
+        <Stack
+            direction={direction || "row"}
+            flexWrap='wrap'
+            justifyContent='start'
+            alignItems='start'
+            gap={2}>
             {videos.map((item, idx) => (
                 <Box key={idx}>
                     {item.id.videoId && <VideoCard video={item} />}
