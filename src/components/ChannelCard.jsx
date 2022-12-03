7859571d247758d4c4d47b3cd4160bfe810543be
ravi raushan <raviraushanweb@gmail.com>
2022-12-03 11:52:18 +0530

    channel profile page

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -4,7 +4,7 @@ import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { Link } from "react-router-dom";
 import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = ({ channelDetail }) => (
+const ChannelCard = ({ channelDetail, marginTop }) => (
     <Box
         sx={{
             boxShadow: "none",
